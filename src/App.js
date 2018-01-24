66ecf468f7e4414a377a7e19aa17b1019f9a043b
SlimTim10 <slimtim10@gmail.com>
2018-01-24 11:03:14 -0500

    Move data into initial state

--- src/App.js
+++ src/App.js
@@ -19,29 +19,13 @@ class Navbar extends Component {
 
 class Messages extends Component {
   render() {
-    const message1 = {
-      date: "May 20 6:00 PM",
-      content: "Bob suggests party at my house on Sat May 27 at 7:00 PM - 10:00 PM",
-      people: [
-        { id: 1, name: "Fred", status: Status.going },
-        { id: 2, name: "Mary", status: Status.going },
-      ]
-    };
-    const message2 = {
-      date: "May 21 10:57 PM",
-      content: "Tim suggests gym on Wed May 24 at 6:30 PM",
-      people: [
-        { id: 3, name: "Bob", status: Status.going },
-        { id: 4, name: "Alice", status: Status.going },
-        { id: 1, name: "Fred", status: Status.notGoing },
-        { id: 2, name: "Mary", status: Status.notGoing },
-      ]
-    };
+    const messages = this.props.messages.map(m => {
+      return <Message key={m.id} data={m} />;
+    });
     
     return (
       <main className="messages">
-        <Message data={message1} />
-        <Message data={message2} />
+        {messages}
       </main>
     );
   }
@@ -80,11 +64,41 @@ class Message extends Component {
 }
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    const message1 = {
+      id: 1,
+      date: "May 20 6:00 PM",
+      content: "Bob suggests party at my house on Sat May 27 at 7:00 PM - 10:00 PM",
+      people: [
+        { id: 1, name: "Fred", status: Status.going },
+        { id: 2, name: "Mary", status: Status.going },
+      ]
+    };
+    const message2 = {
+      id: 2,
+      date: "May 21 10:57 PM",
+      content: "Tim suggests gym on Wed May 24 at 6:30 PM",
+      people: [
+        { id: 3, name: "Bob", status: Status.going },
+        { id: 4, name: "Alice", status: Status.going },
+        { id: 1, name: "Fred", status: Status.notGoing },
+        { id: 2, name: "Mary", status: Status.notGoing },
+      ]
+    };
+    
+    this.state = {
+      currentUser: {name: "Tim"},
+      messages: [message1, message2]
+    };
+  }
+  
   render() {
     return (
       <div className="App">
-        <Navbar username="Tim" />
-        <Messages />
+        <Navbar username={this.state.currentUser.name} />
+        <Messages messages={this.state.messages} />
       </div>
     );
   }
