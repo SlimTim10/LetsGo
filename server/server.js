2a45d4e80cb42429a307f4b6cd5937c1f868484e
SlimTim10 <slimtim10@gmail.com>
2018-02-02 10:33:02 -0500

    Create server

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const SocketServer = require('ws').Server;
+
+const PORT = 3001;
+
+const server = express()
+  .use(express.static('public'))
+  .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`));
+
+const wss = new SocketServer({ server });
+
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+
+  ws.on('message', (message) => {
+    console.log('received: %s', JSON.parse(message));
+  });
+
+  ws.on('close', () => console.log('Client disconnected'));
+});
